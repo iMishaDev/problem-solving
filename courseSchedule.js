b5bfe23d443be53607fbcf9e8d27bd02fde2c27a
Mashael <m.alsaeedi@safcsp.org.sa>
2021-08-17 15:37:38 +0300

    improve day#3 problem#2 solution

--- courseSchedule.js
+++ courseSchedule.js
@@ -1,12 +1,18 @@
 class Solution {
-    static findCycle(courses){
+
+    /**
+     * complexity: O(n^2)
+     * @param {Array} courses 
+     * @returns {Boolean}
+     */
+    static findCycle_1(courses){
         let coursesSheet = this.#constructSheet(courses);
         let hasCycle = false;
 
         
-        for (const key in coursesSheet){
-            coursesSheet[key].forEach((num) => {
-                if (coursesSheet[num] && coursesSheet[num].includes(Number(key)))
+        for (const course in coursesSheet){
+            coursesSheet[course].forEach((num) => {
+                if (coursesSheet[num] && coursesSheet[num].includes(Number(course)))
                     hasCycle = true;
             })
         }
@@ -14,13 +20,58 @@ class Solution {
         return hasCycle;
     }
 
+    /**
+     * complexity: O(n)
+     * @param {Array} courses 
+     * @returns {Boolean}
+     */
+    static findCycle_2(courses){
+        let coursesSheet = this.#constructSheet(courses);
+
+        
+        for (const course in coursesSheet){
+            if(this.#has_cycle(coursesSheet,course, new Set(), {}))
+                return true;
+        }
+
+        return false;
+    }
+
+    static #has_cycle(sheet, course, seen, cache){
+        let found = false;
+
+        if(cache[course])
+            return cache[course];
+        
+        if(seen.has(course))
+            return true;
+        
+        if(!sheet[course])
+            return false;
+        
+        seen.add(course);
+        
+        
+        for(const c of sheet[course])
+            if(this.#has_cycle(sheet, c, seen, cache)){
+                found = true;
+                break;
+            }
+                
+        cache[course] = found;
+
+        return found;
+    }
+
     static #constructSheet(courses){
+        let coursesSheet = {};
         for (const course of courses){
             if (coursesSheet[course[0]])
                 coursesSheet[course[0]].push(course[1]);
             else coursesSheet[course[0]] = [course[1]];
         }
+        return coursesSheet;
     }
 }
 
-console.log('find Cycle ', Solution.findCycle([[0,1], [0,3], [1,0]]));
+console.log('find Cycle ', Solution.findCycle_2([[0,1], [1,0]]));
