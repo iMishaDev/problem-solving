9e1bb6b606a075af261a4f8381ce0d07e07e4a48
Mashael <m.alsaeedi@safcsp.org.sa>
2021-08-18 17:35:35 +0300

    solve day#4 problem#6

---
+++ validParentheses.js
@@ -0,0 +1,24 @@
+class Solution {
+    static validParentheses(text){
+        let stack = [];
+        let parenthesesList = text.split('');
+        const shiftedParentheses = new Set(['}', ')', ']']);
+
+        const shiftedParenthesesSheet =  {
+            '}':'{',
+            ')': '(',
+            ']':'['
+        };
+
+        for (const letter of parenthesesList){
+            if(shiftedParentheses.has(letter)){
+                if (stack.pop() !== shiftedParenthesesSheet[letter])
+                    return false;
+            } else stack.push(letter);
+        }
+        return stack.length === 0;
+    }
+}
+
+console.log('valid parentheses', Solution.validParentheses('(){([])}'))
+console.log('valid parentheses', Solution.validParentheses('(){(['))
