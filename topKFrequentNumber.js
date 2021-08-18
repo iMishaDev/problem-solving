874ed39e28ff6ab88bf7c55db9b1c74094a1f4ef
Mashael <m.alsaeedi@safcsp.org.sa>
2021-08-18 15:37:56 +0300

    solve day#4 problem#4

---
+++ topKFrequentNumber.js
@@ -0,0 +1,17 @@
+import { createSheet, Heap } from './helpers.js';
+class Solution {
+    static topKFrequentNumber(numbers, k){
+        let sheet = createSheet();
+        for(const number of numbers){
+            sheet[number] += 1;
+        }
+
+        let heap =  new Heap();
+        for (const key in sheet){
+            heap.add({[key]: sheet[key]});
+        }
+        return heap.findTheHighestKthElement(k)
+    }
+}
+
+console.log('Top 2 frequent numbers', Solution.topKFrequentNumber([1, 1, 1, 2, 2, 3, ], 2))
