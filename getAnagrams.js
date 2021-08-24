4b4946c99aeedbfebe7eec648caf5e86a8abbd24
Mashael <m.alsaeedi@safcsp.org.sa>
2021-08-24 10:13:52 +0300

    solve day#8 problem#6 approach#1 enhancment

--- getAnagrams.js
+++ getAnagrams.js
@@ -1,33 +1,25 @@
 class Solution {
     static getWordsAnagrams(words){
-        let sheet = {
-            "a": 1,
-            "b": 2,
-            "c": 3,
-            "d": 4,
-            "e": 5,
-            "f": 6,
-            "g": 7
-        };
         let wordsSheet = {};
-        let results = [];
-
-        for(let word of words){
+        
+        function hashFun(word){
             let wordValue = 0;
+
             word = word.split('');
             for (const letter of word){
-                wordValue += sheet[letter];
+                wordValue += letter.charCodeAt(0);
             }
+            return wordValue;
+        }
+
+        for(let word of words){
+            let wordValue = hashFun(word)
             if(!wordsSheet[wordValue])
-                wordsSheet[wordValue] = [word.join('')];
-            else wordsSheet[wordValue].push(word.join(''))
+                wordsSheet[wordValue] = [word];
+            else wordsSheet[wordValue].push(word)
         }
 
-        for (const key in wordsSheet){
-            results.push(wordsSheet[key]);
-        }
-
-        return results;
+        return Object.values(wordsSheet);
     }
 }
 
