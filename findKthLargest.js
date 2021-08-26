385c97784b125f451594b7ebaf264a5f77c604bd
Mashael <m.alsaeedi@safcsp.org.sa>
2021-08-26 13:16:04 +0300

    solve day#11 problem#1 approach#1

--- findKthLargest.js
+++ findKthLargest.js
@@ -2,11 +2,31 @@ import { Heap } from './helpers.js'
 
 
 class Solution {
+    /**
+     * find the Kth element using Heap Data Structure 
+     * @param {Array} numbers 
+     * @param {Integer} k 
+     * @returns 
+     */
     static findKthLargest(numbers, k){
         let heap = new Heap(numbers);
         return heap.findTheHighestKthElement(k)
     }
+
+
+    /**
+     * find the Kth element using JS sort function which uses (MergeSort).
+     * @param {Array} numbers 
+     * @param {Integer} k 
+     * @returns 
+     */
+    static iterativeFindKthElement_1(numbers, k){
+        numbers = numbers.sort((a,b) => a-b);
+        return numbers[k];
+    }
+
+    
 }
 
 
-console.log('Kth Largest Element: ', Solution.findKthLargest([3, 5, 2, 4, 6, 8], 3))
+console.log('Kth Largest Element: ', Solution.iterativeFindKthElement_1([3, 5, 2, 4, 6, 8], 3))
