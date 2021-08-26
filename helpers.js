fe0004e058ff707068dfddbb7ec1b0bb9c5f0039
Mashael <m.alsaeedi@safcsp.org.sa>
2021-08-26 13:58:24 +0300

    renamce tree node to binary tree node, add a tree node and queue classes

--- helpers.js
+++ helpers.js
@@ -23,7 +23,7 @@ export const negativeIndexedArray = () =>  {
     }
     return new Proxy([], handler);
 }
-export class TreeNode {
+export class BinaryBinaryTreeNode {
 
 
 /**
@@ -40,6 +40,50 @@ export class TreeNode {
     }
 }
 
+
+
+export class TreeNode {
+
+
+/**
+ * @constructor 
+ * @param {number} value 
+ * @param {Array} children 
+ */
+
+    constructor(value, children=[]){
+        this.value = value
+        this.children = children
+    }
+}
+
+
+export class Queue {
+    constructor(){
+        this.queue = [];
+        this.tail = 0;
+        this.head = 0;
+    }
+
+    enqueue(element) {
+        this.queue = [...this.queue, element];
+        this.tail = element
+    }
+
+    size() {
+        return this.queue.filter((element) => element !== null).length
+    }
+
+    peek(){
+        return this.queue[this.head];
+    }
+
+    dequeue() {
+        let element = this.queue.shift();
+        this.head +=1;
+        return element;
+    }
+}
 
 export class LinkedListNode {
 
