bdf20aeb9c38e8e05bbe575490fa08240a091d05
Mashael <m.alsaeedi@safcsp.org.sa>
2021-08-26 13:16:28 +0300

    solve day#11 problem#1 approach#2

--- findKthLargest.js
+++ findKthLargest.js
@@ -25,8 +25,22 @@ class Solution {
         return numbers[k];
     }
 
-    
+    static iterativeFindKthElement_2(numbers, k){
+        let largest = [Number.MIN_VALUE, -1];
+
+        while(k){
+            largest = [Number.MIN_VALUE, -1];
+            for(const [key, number] of numbers.entries()){
+                largest = [Math.max(largest[0], number), key];
+            }
+            numbers.splice(largest[1], 1)
+            k -= 1;
+        }
+        return largest[0];
+    }
+
+
 }
 
 
-console.log('Kth Largest Element: ', Solution.iterativeFindKthElement_1([3, 5, 2, 4, 6, 8], 3))
+console.log('Kth Largest Element: ', Solution.iterativeFindKthElement_3([3, 5, 2, 4, 6, 8], 3))
