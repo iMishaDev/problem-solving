183ca428b279b9254d5918ed90cfa48370eb901f
Mashael <m.alsaeedi@safcsp.org.sa>
2021-08-26 13:46:31 +0300

    solve day#11 problem#2 approach#2

--- cloneTree.js
+++ cloneTree.js
@@ -2,7 +2,6 @@ import {TreeNode} from './helpers.js';
 
 class Solution {
     static findNode(tree, cloneTree , n){
-        console.log(cloneTree.value)
         return this.#findNodeHelper(tree, cloneTree, n);
     }
 
@@ -17,6 +16,30 @@ class Solution {
 
         return null;
     }
+
+    static findNodeIterative(tree, cloneTree, n){
+        let stack = [tree];
+        let stackClone = [cloneTree];
+
+        while(stack.length && stackClone.length){
+            let node = stack.pop();
+            let clonedNode = stackClone.pop();
+
+            if(node == n)
+                return clonedNode;
+            
+            if(node.leftChild && clonedNode.leftChild){
+                stack.push(node.leftChild);
+                stackClone.push(clonedNode.leftChild);
+            }
+
+            if(node.rightChild && clonedNode.rightChild){
+                stack.push(node.rightChild);
+                stackClone.push(clonedNode.rightChild);
+            }
+        }
+        return null;
+    }
 }
 
 const validBinaryTree = new TreeNode(10,
@@ -27,4 +50,4 @@ const validBinaryTreeClone = new TreeNode(10,
                     new TreeNode(5, new TreeNode(4,null), new TreeNode(8, null)),
                     new TreeNode(20, new TreeNode(18,null),new TreeNode(24, new TreeNode(22, null))));
 
-console.log('find the cloned node', Solution.findNode(validBinaryTree, validBinaryTreeClone , validBinaryTree.leftChild.leftChild))
+console.log('find the cloned node', Solution.findNodeIterative(validBinaryTree, validBinaryTreeClone , validBinaryTree.leftChild.leftChild))
