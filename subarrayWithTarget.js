da00e03e73b387ba4b6b0a5b94fc3302017b37c6
Mashael <m.alsaeedi@safcsp.org.sa>
2021-08-30 19:21:49 +0300

    solve day#12 problem#4 approach#2

--- subarrayWithTarget.js
+++ subarrayWithTarget.js
@@ -15,6 +15,20 @@ class Solution {
 
         return numbers.slice(start, end);
     }
+
+    static getSubArrayV2(numbers, target){
+        let sheet = {};
+        let sum = 0;
+
+        for(let i =0; i<numbers.length; i++){
+
+            sum += numbers[i];
+            if(sheet[sum - target])
+                return numbers.slice(numbers[sheet[sum - target]], numbers[i]);
+            sheet[sum] = i;
+        }
+
+    }
 }
 
-console.log('get sub array', Solution.getSubArray([1, 3, 2, 5, 7, 2], 14))
+console.log('get sub array', Solution.getSubArrayV2([1, 3, 2, 5, 7, 2], 14))
