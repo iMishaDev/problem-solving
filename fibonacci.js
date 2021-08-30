80abd1f13fde968323548a4c584f872c88eff671
Mashael <m.alsaeedi@safcsp.org.sa>
2021-08-30 17:30:53 +0300

    solve day#12 problem#2 approach#2

--- fibonacci.js
+++ fibonacci.js
@@ -8,6 +8,26 @@ class Solution {
         return this.fibonacci(number - 1) + this.fibonacci(number - 2)
     }
 
+
+    static fibonacciIterative(number){
+        let sum = 0;
+        let stack = [0, 1];
+
+        let i = number - 2;
+
+        while(i > -1){
+            let num1 = stack.pop();
+            let num2 = stack.pop();
+
+            sum = num1 + num2;
+
+            stack.push(num1);
+            stack.push(sum);
+            i -= 1
+        }
+
+        return sum;
+    }
 }
 
-console.log('fibonacci', Solution.fibonacci(10))
+console.log('fibonacci', Solution.fibonacciIterative(10))
