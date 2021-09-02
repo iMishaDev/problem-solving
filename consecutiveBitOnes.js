0cd0df3536eb9ef2d29850bc194f5bd995323091
Mashael <m.alsaeedi@safcsp.org.sa>
2021-09-02 15:11:03 +0300

    solve day#14 problem#3

---
+++ consecutiveBitOnes.js
@@ -0,0 +1,31 @@
+class Solution {
+    static findLongestBitOnes(number){
+        let i = 0;
+        let calculatedNumber = number;
+        let binaryNumber = '';
+        let lengths = [];
+        let length = 0;
+
+        while(calculatedNumber > 0){
+            if(calculatedNumber&1)
+                binaryNumber += '1'
+            else binaryNumber += '0'
+            calculatedNumber = calculatedNumber >> 1;
+            i += 1
+        }
+
+        for (const num of binaryNumber.split('')){
+            if (num === '1')
+                length += 1;
+            else {
+                lengths.push(length);
+                length = 0;
+            }
+        }
+        lengths.push(length);
+
+        return Math.max(...lengths);
+    }
+}
+
+console.log('findLongestBitOnes of 242 ', Solution.findLongestBitOnes(242))
