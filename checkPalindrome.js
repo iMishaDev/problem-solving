c867ace8391ace953bc3b17760250448f5a2575e
Mashael <m.alsaeedi@safcsp.org.sa>
2021-09-02 14:14:29 +0300

    solve day#14 problem#1

---
+++ checkPalindrome.js
@@ -0,0 +1,25 @@
+class Solution {
+    static findPalindrome(text){
+        let sheet = {};
+        let letters = text.split('');
+        let result = '';
+        let odd = '';
+
+        for (const letter of letters){
+            if(sheet[letter])
+                sheet[letter] += 1;
+            else sheet[letter] = 1;
+        }
+
+        for (const key in sheet){
+            if(sheet[key] % 2 == 0)
+                result += key
+            else if (odd === '')
+                odd = key;
+            else return false;
+        } 
+        return `${result}${odd}${result}`;
+    }
+}
+
+console.log('find palindrome', Solution.findPalindrome('foxfo'))
