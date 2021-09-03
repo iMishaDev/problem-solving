b453bc733501af8f96f2fecf53cf147090a8489d
Mashael <m.alsaeedi@safcsp.org.sa>
2021-09-03 12:51:04 +0300

    solve day#15 problem#3 approach#2

--- optimizedListSum.js
+++ optimizedListSum.js
@@ -2,8 +2,21 @@ class ListFastSum {
 
     constructor(numbers){
         this.numbers = numbers;
+        this.sums = this.#constructSums(numbers)
     }
     
+    #constructSums(numbers){
+        let sum = 0;
+        return numbers.map((number) => sum += number)
+    }
+
+/**
+ * Time complexity: O(n)
+ * Space Complexity: O(1)
+ * @param {Integer} start 
+ * @param {Integer} end 
+ * @returns Integer
+ */
     sum(start, end){
         let sum = 0;
         for(let i = start; i < end; i++){
@@ -12,6 +25,21 @@ class ListFastSum {
 
         return sum;
     }
+
+
+
+/**
+ * Time complexity: O(1)
+ * Space Complexity: O(n)
+ * @param {Integer} start 
+ * @param {Integer} end 
+ * @returns Integer
+ */
+    sum2(start, end){
+        return this.sums[end - 1] - this.sums[start - 1];
+    }
 }
 
+
 console.log('List Fast Sum', new ListFastSum([1, 2, 3, 4, 5, 6, 7]).sum(2, 5))
+console.log('List Fast Sum', new ListFastSum([1, 2, 3, 4, 5, 6, 7]).sum2(2, 5))
