5a1640dfe8907386211be151594ce2ee2446b744
Mashael <m.alsaeedi@safcsp.org.sa>
2021-09-03 12:02:38 +0300

    solve day#14 problem#6 #approach#3

--- inorderSucessor.js
+++ inorderSucessor.js
@@ -14,7 +14,24 @@ class Solution {
 
         
         return  Math.max(root.parent.value, this.#inOrderSuccessorHelper(root.parent));
+    }
+
+
+    static inOrderSuccessorIterative(root){
+        if (root.rightChild){
+            let curr = root.rightChild
+            while (curr.leftChild)
+                curr = curr.leftChild
+            return curr.value
+        }
         
+        let curr = root
+        let parent = curr.parent
+        while (parent && parent.leftChild != curr){
+            curr = parent
+            parent = parent.parent
+        }
+        return parent.value
     }
 }
 
@@ -36,5 +53,9 @@ tree.rightChild.leftChild.rightChild.parent = tree.rightChild.leftChild
 tree.rightChild.rightChild = new BinaryTreeNode(9)
 tree.rightChild.rightChild.parent = tree.rightChild
 
-console.log(Solution.inOrderSuccessor(tree.leftChild.leftChild))
-console.log(Solution.inOrderSuccessor(tree.rightChild.leftChild.rightChild))
+console.log('successor of 1',Solution.inOrderSuccessor(tree.leftChild.leftChild))
+console.log('successor of 7',Solution.inOrderSuccessor(tree.rightChild.leftChild.rightChild))
+
+
+console.log('successor of 1',Solution.inOrderSuccessorIterative(tree.leftChild.leftChild))
+console.log('successor of 7',Solution.inOrderSuccessorIterative(tree.rightChild.leftChild.rightChild))
