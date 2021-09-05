636b936107636f92903b420b8a60a2b5bb7dbf79
Mashael <m.alsaeedi@safcsp.org.sa>
2021-09-05 11:42:30 +0300

    solve day#17 problem#6 #approach#2

--- isPalindrome.js
+++ isPalindrome.js
@@ -12,7 +12,24 @@ class Solution {
         
         return text[start] === text[end] && this.#isPalindromeHelper(text, start + 1, end - 1);
     }
+
+    static isPalindromeIterative(text){
+        let isPalindrome = true;
+        text = text.replace(/[^A-Za-z0-9]/g, '').toLowerCase();
+        
+        for(let i = 0, j = text.length -1 ; i <= text.length/2, j >= text.length/2; i++, j--){
+            if(text[i] !== text[j])
+                isPalindrome = false;
+        }
+
+        return isPalindrome;
+    }
 }
 
-console.log('isPalindrome', Solution.isPalindrome('Bob'))
+console.log('isPalindrome', Solution.isPalindrome('Boab'))
 console.log('isPalindrome', Solution.isPalindrome('A man, a plan, a canal – Panama'))
+
+
+
+console.log('isPalindrome', Solution.isPalindromeIterative('Boab'))
+console.log('isPalindrome', Solution.isPalindromeIterative('A man, a plan, a canal – Panama'))
