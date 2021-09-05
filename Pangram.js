8e90eea5ecf44a775c40903ee0c070196b830bf3
Mashael <m.alsaeedi@safcsp.org.sa>
2021-09-05 09:34:36 +0300

    solve day#17 problem#4

---
+++ Pangram.js
@@ -0,0 +1,18 @@
+class Solution {
+    static isPangram(word){
+
+        if(!word || word.length < 26) 
+            return false;
+        
+        let sheet = new Map();
+        word = word.replace(/[.,\s]/g, '')
+
+        for(const letter of word.split('')){
+            sheet.set(letter.toLowerCase(), 1);
+        }
+
+        return sheet.size <= 33;
+    }
+}
+
+console.log('Pangram', Solution.isPangram('Waltz, bad nymph, for quick jigs vex.'))
