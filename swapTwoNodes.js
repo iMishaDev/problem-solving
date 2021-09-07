24ab3e8ae43c9d36f177cad5008287703c7efa07
Mashael <m.alsaeedi@safcsp.org.sa>
2021-09-07 23:12:53 +0300

    solve day#19 problem#2

---
+++ swapTwoNodes.js
@@ -0,0 +1,16 @@
+import { LinkedListNode} from './helpers.js';
+
+class Solution {
+    static swap(head){
+        let current = head;
+        while(current.next){
+            [current.value, current.next.value] = [current.next.value, current.value];
+            current = current.next.next;
+        }
+
+        return head;
+    }
+}
+let node = new LinkedListNode(1, new LinkedListNode(2, new LinkedListNode(3, new LinkedListNode(4,new LinkedListNode(5) ))))
+
+console.log(Solution.swap(node))
