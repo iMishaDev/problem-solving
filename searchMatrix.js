75901c69480d0a2aabac1719de4fed8398e5f72f
Mashael <m.alsaeedi@safcsp.org.sa>
2021-09-08 11:50:04 +0300

    solve day#20 problem#4 approach#2

--- searchMatrix.js
+++ searchMatrix.js
@@ -3,6 +3,10 @@ class Solution {
         return this.#searchHelper(matrix,0, 0, target);
     }
 
+    search2(matrix, target){
+        return this.#searchHelper2(matrix, target);
+    }
+
 /**
  * Assuming the matrix is not sorted
  * @param {Array} matrix 
@@ -20,6 +24,32 @@ class Solution {
         
         return this.#searchHelper(matrix, col, row + 1, target) || this.#searchHelper(matrix, col + 1, row, target);
     }
+
+
+/**
+ * Assuming the matrix is  sorted
+ * @param {Array} matrix 
+ * @param {Boolean} target 
+ * @returns 
+ */
+    #searchHelper2(matrix, target){
+        let low = 0;
+        let high = matrix.length * matrix[0].length;
+        while(low < high){
+            let mid = Math.floor((low + high ) / 2);
+            
+            let mid_val = matrix[Math.floor(mid / matrix.length)][mid % matrix.length];
+            if( mid_val == target)
+                return true;
+            else if(mid_val < target){
+                low = mid + 1;
+            }else {
+                high = mid;
+            }
+        }
+
+        return false;
+    }
 }
 
 const matrix = [
@@ -29,3 +59,4 @@ const matrix = [
 ];
 
 console.log(new Solution().search(matrix, 10))
+console.log(new Solution().search2(matrix, 10))
