5e8cebb2ca674f90870df9d96e1647e4dd5032d1
Mashael <m.alsaeedi@safcsp.org.sa>
2021-09-08 12:28:09 +0300

    solve day#20 problem#5 approach#2

--- findCousins.js
+++ findCousins.js
@@ -8,6 +8,8 @@ class Solution {
             let node = queue.shift();
         
             if(node == n){
+                if(queue.length > 2)
+                    queue.shift();
                 return queue;
             }
 
@@ -21,6 +23,31 @@ class Solution {
 
         return [];
     }
+
+
+    findCousins2(root, n){
+        let [height, parent] = this.#findHeight(root, null, n, 0)
+        let cousins = this.#getNodeWithHeight(root, 0, height, parent);
+
+        return cousins;
+    }
+
+    #getNodeWithHeight(root, height, target, exclude){
+        if(!root || root === exclude)
+            return [];
+        if(height === target)
+            return [root];
+        return [...this.#getNodeWithHeight(root.leftChild, height + 1, target, exclude), ...this.#getNodeWithHeight(root.rightChild, height + 1, target, exclude)]
+    }
+
+
+    #findHeight(root, parent, n, height){
+        if(!root)
+            return;
+        if(root === n)
+            return [height, parent];
+        return (this.#findHeight(root.leftChild, root, n, height + 1) || this.#findHeight(root.rightChild, root, n, height + 1))
+    }
 }
 //      1
 //     / \
@@ -33,6 +60,7 @@ root.leftChild.leftChild = new BinaryTreeNode(4)
 root.leftChild.rightChild = new BinaryTreeNode(6)
 root.rightChild = new BinaryTreeNode(3)
 root.rightChild.rightChild = new BinaryTreeNode(5)
-
+root.rightChild.leftChild = new BinaryTreeNode(7)
 
 console.log(new Solution().findCousins(root, root.rightChild.rightChild))
+console.log(new Solution().findCousins2(root, root.rightChild.rightChild))
