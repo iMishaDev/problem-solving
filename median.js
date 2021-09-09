76a8393be863d396e589f48aa920c6f52f3c44f7
Mashael <m.alsaeedi@safcsp.org.sa>
2021-09-09 11:39:22 +0300

    solve day#21 problem#2 #fix

--- median.js
+++ median.js
@@ -3,8 +3,13 @@ class Solution {
         let medians = [];
         for(let i = 0; i < numbers.length; i++){
             let sub = numbers.slice(0, i + 1);
-            let sum = sub.reduce((val, newval) => val+newval)
-            medians[i] = sum/2;
+            sub.sort((a, b) => a - b);
+
+            let index = Math.floor(sub.length/ 2);
+            
+            if(!sub.length % 2)
+                medians[i] = sub[index];
+            else medians[i] = (sub[index - 1] + sub[index]) / 2.0 || sub[0]
         }
 
         return medians;
