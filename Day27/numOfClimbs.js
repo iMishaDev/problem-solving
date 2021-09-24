adcafa5738fb78516b84006205ca38e027edbdb4
Mashael <m.alsaeedi@safcsp.org.sa>
2021-09-24 10:28:32 +0300

    add memoized solution

--- Day27/numOfClimbs.js
+++ Day27/numOfClimbs.js
@@ -24,32 +24,72 @@ class Solution {
     #numOfUniqueHelper(n, sum){
         if(sum > n)
             return 0;
+        
         if(sum === n)
             return 1;
         
         return this.#numOfUniqueHelper(n, sum + 1) + this.#numOfUniqueHelper(n, sum + 2)
     }
+// /**
+//  * better approach
+//  * @param {*} n 
+//  * @returns 
+//  */
+//     #numOfUniqueHelper(n){
+//         if(n < 0)
+//             return 0;
+        
+//         if(n === 0)
+//             return 1;
+        
+//         return this.#numOfUniqueHelper(n - 1) + this.#numOfUniqueHelper(n - 2)
+//     }
 
 
     numOfUniqueV2(n, steps){
-        return this.#numOfUniqueHelperV2(n, 0,  steps, 0);
+        return this.#numOfUniqueHelperV2(n,  steps);
     }
 
-    #numOfUniqueHelperV2(n, sum, steps){
-        if(sum > n)
+    #numOfUniqueHelperV2(n, steps){
+        if(n < 0)
             return 0;
         
-        if(sum === n)
+        if(n === 0)
             return 1;
+        
         let count = 0;
         
         for (const step of steps){
-            count += this.#numOfUniqueHelperV2(n, sum + step, steps)
+            count += this.#numOfUniqueHelperV2(n - step, steps)
         }
 
+
         return count;
+    }
+
+
+
+    numOfUniqueV2C(n, steps){
+        return this.#numOfUniqueHelperV2C(n,  steps, new Array(n + 1).fill(0));
+    }
+    
+    #numOfUniqueHelperV2C(n, steps, cache){
+        cache[0] = 1
+        
+        let sum = 0;
+        for (let i = 1; i <= n; i++){
+            sum = 0;
+            for(const step of steps){
+                if(i - step >= 0)
+                    sum += cache[i - step];
+            }
+            cache[i] += sum;
+        }
+
+        return cache[n];
     }
 }
 
 console.log(new Solution().numOfUnique(4))
 console.log(new Solution().numOfUniqueV2(4, [1, 3, 5]))
+console.log(new Solution().numOfUniqueV2C(4, [1, 3, 5]))
