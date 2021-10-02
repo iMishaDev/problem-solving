4ed14e2ae8e170844264ccc223c941ef5379e1e2
Mashael <m.alsaeedi@safcsp.org.sa>
2021-10-02 15:44:47 +0300

    solve day#33 problem#3

---
+++ Day33/balancedBrackets.js
@@ -0,0 +1,36 @@
+/**
+ * Given a string of round, curly, and square open and closing brackets,
+ *  return whether the brackets are balanced (well-formed).
+
+For example, given the string "([])[]({})", you should return true.
+
+Given the string "([)]" or "((()", you should return false.
+ */
+
+class Solution {
+    isBalanced(brackets){
+        let stack = [];
+
+        for(const element of brackets){
+            if(element === '(' || element === '[' || element === '{')
+                stack.push(element);
+            else if (element === ')'){
+                if(stack[stack.length - 1] === '(')
+                    stack.pop();
+            }
+            else if (element === ']'){
+                if(stack[stack.length - 1] === '[')
+                    stack.pop();
+            }
+            else if (element === '}'){
+                if(stack[stack.length - 1] === '{')
+                    stack.pop();
+            }
+        }
+        
+        return stack.length == 0;
+    }
+}
+
+console.log(new Solution().isBalanced('([])[]({})'))
+console.log(new Solution().isBalanced('([)]'))
