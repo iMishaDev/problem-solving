e451615519650e11cd69824f3692fa1ee41b9ea1
Mashael <m.alsaeedi@safcsp.org.sa>
2021-10-24 22:23:29 +0300

    solve day#38 problem#2 #memoization

--- Day38/palindrome.js
+++ Day38/palindrome.js
@@ -6,22 +6,38 @@ class Solution {
 
 
     check_palindrome(text){
+        return this.check_palindrome_helper(text, {});
+    }
+
+
+    check_palindrome_helper(text, cache){
         if(this.reverse(text) == text){
+            cache[text] = text;
             return text;
         }
         else if (text[0] == text[text.length - 1]){
-            return text[0] + this.check_palindrome(text.slice(1, -1)) + text[text.length - 1];
+            let t = text[0] + this.check_palindrome_helper(text.slice(1, -1), cache) + text[text.length - 1];
+            cache[text] = t;
+            return t;
         } else {
-            let first = text[0] + this.check_palindrome(text.slice(1)) + text[0];
-            let second = text[text.length-1] + this.check_palindrome(text.slice(0, -1)) + text[text.length-1]
+            let first = text[0] + this.check_palindrome_helper(text.slice(1), cache) + text[0];
+            let second = text[text.length-1] + this.check_palindrome_helper(text.slice(0, -1), cache) + text[text.length-1]
 
-            if(first.length < second.length)
+            if(first.length < second.length){
+                cache[text] = first;
                 return first;
-            else if(first.length > second.length)
+            }
+            else if(first.length > second.length){
+                cache[text] = second
                 return second;
-            else if(first > second)
+            }
+            else if(first > second){
+                cache[text] = second;
                 return second;
-            else return first;
+
+            }
+            cache[text] = first;
+            return first;
         }
         
     }
