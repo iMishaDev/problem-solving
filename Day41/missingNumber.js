12d368e6cca9bf10363deb600a8df5609b43424e
Mashael <m.alsaeedi@safcsp.org.sa>
2021-10-30 23:28:12 +0300

    solve day#41 problem#7 #fix

--- Day41/missingNumber.js
+++ Day41/missingNumber.js
@@ -33,7 +33,7 @@ Explanation: n = 1 since there is 1 number, so all numbers are in the range
 
 class Solution {
     get_missing(nums){
-        nums.sort();
+        nums.sort((a, b) => a - b);
         let missing = 1;
         for(const num of nums){
             if (num == missing)
@@ -77,7 +77,7 @@ class Solution {
     }
 }
 
-console.log(new Solution().get_missing([9,6,4,2,3,5,7,0,1]))
+console.log(new Solution().get_missing([45,35,38,13,12,23,48,15,44,21,43,26,6,37,1,19,22,3,11,32,4,16,28,49,29,36,33,8,9,39,46,17,41,7,2,5,27,20,40,34,30,25,47,0,31,42,24,10,14]))
 console.log(new Solution().get_missing_2([9,6,4,2,3,5,7,0,1]))
 console.log(new Solution().get_missing_3([9,6,4,2,3,5,7,0,1]))
 console.log(new Solution().get_missing_4([9,6,4,2,3,5,7,0,1]))
