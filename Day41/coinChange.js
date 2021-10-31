0798379b63581a140d60ed12e88c95f1e9e1e776
Mashael <m.alsaeedi@safcsp.org.sa>
2021-10-31 10:45:09 +0300

    solve day#41 problem#10 #apprach 2

--- Day41/coinChange.js
+++ Day41/coinChange.js
@@ -33,6 +33,13 @@ Output: 2
  */
 
 class Solution {
+
+    /**
+     * Greedy approach
+     * @param {Array} coins 
+     * @param {Integer} amount 
+     * @returns 
+     */
     get_change(coins, amount){
         let total = 0;
         let count = 0;
@@ -40,9 +47,13 @@ class Solution {
         coins.sort((a, b) => a - b)
         while(total < amount){
             coin = this.get_coin(coins, amount - total);
-            total += coin;
-            console.log(total, coin)
-            count += 1;
+            if(!coin) return -1;
+            if(total + coin > amount){
+                return -1;
+            }else {
+                total += coin;
+                count += 1;
+            }
         } 
 
         return count;
@@ -54,6 +65,40 @@ class Solution {
             i -= 1
         return coins[i];
     }
+
+
+    /**
+     * Optimal Solution
+     * @param {*} coins 
+     * @param {*} amount 
+     * @returns 
+     */
+
+    get_change_dynamic_programming(coins, amount){
+        let calcs = new Array(amount + 1).fill(amount + 1);
+        calcs[0] = 0;
+
+        for(let i = 1; i < amount + 1; i++){
+            for(const coin of coins){
+
+                if (i - coin >= 0){
+                    calcs[i] = Math.min(calcs[i], 1 + calcs[i - coin]);
+                }
+            }
+        }
+
+        return calcs[amount]
+    }
 }
 
-console.log(new Solution().get_change([1,2,5], 11))
+
+console.log(new Solution().get_change_dynamic_programming([1, 3, 4, 5], 7))
+console.log(new Solution().get_change([2], 3))
+console.log(new Solution().get_change([1], 0))
+console.log(new Solution().get_change([1], 1))
+console.log(new Solution().get_change([1], 2))
+console.log(new Solution().get_change([186,419,83,408], 6249))
+console.log(new Solution().get_change_dynamic_programming([186,419,83,408], 6249))
+
+
+
