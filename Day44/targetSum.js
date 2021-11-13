8be2b95678f13c342721a90fb681bedbc689aa7e
Mashael <m.alsaeedi@safcsp.org.sa>
2021-11-13 08:12:57 +0300

    add memoization

--- Day44/targetSum.js
+++ Day44/targetSum.js
@@ -4,16 +4,17 @@ class Solution {
     }
 
     sum_helper(numbers, target){
-
+        let cache = {}
         return (function sum_sum_helper(index, total){
             if(index === numbers.length){
-                return total === target ? 1: 0;
+                cache[[index, total]] = total === target ? 1: 0;
+                return cache[[index, total]];
             }
             
-                return sum_sum_helper(index + 1, total + numbers[index])
-                + sum_sum_helper(index + 1, total - numbers[index])
+            cache[[index, total]] = sum_sum_helper(index + 1, total + numbers[index])
+                + sum_sum_helper(index + 1, total - numbers[index]);
+            return cache[[index, total]];
         }(0, 0));
-        
     }
 }
 
